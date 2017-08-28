9610fe153f971e30e461f22a2c795af8b03dcacb
Enes <30447759+pesh12@users.noreply.github.com>
2017-08-28 10:16:58 +0200

    Add files via upload
    3 already initialized variables are compared with if statements.

---
+++ sameType.js
@@ -0,0 +1,25 @@
+let var1 = "Hello";
+let var2 = 6;
+let var3 = "World";
+
+let r1 = typeof(var1);
+let r2 = typeof(var2);
+let r3 = typeof(var3);
+
+if(r1 === r2){
+    console.log(var1 +" and " + var2 + " are SAME type.")
+}else{
+    console.log(var1 +" and " + var2 + " are DIFFERENT type.")
+}
+
+if(r1 === r3){
+    console.log(var1 +" and " + var3 + " are SAME type.")
+}else{
+    console.log(var1 +" and " + var3 + " are DIFFERENT type.")
+}
+
+if(r2 === r3){
+    console.log(var2 +" and " + var3 + " are SAME type.")
+}else{
+    console.log(var2 +" and " + var3 + " are DIFFERENT type.")
+}
