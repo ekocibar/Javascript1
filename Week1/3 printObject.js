b4d4bccfe207e097d9021aa19d0179f3ac260ea4
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-04 11:38:19 +0200

    function that outputs keys and values of a object

---
+++ Week1/3 printObject.js
@@ -0,0 +1,16 @@
+function printObject(car){
+    
+    for(let c in car){
+    console.log(c + " " + car[c]);    
+    }
+    return;
+}
+
+let car = {
+        model: "VW",
+        color: "red",
+        year: 2016,
+        goodConditon: true
+    }
+ 
+printObject(car);
