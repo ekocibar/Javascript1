8014d248f191b6c3a340b7f18215c77e870f2c92
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-05 20:25:30 +0200

    Update Week 0 ~ canI.js

--- Week 0/Week 0 ~ canI.js
+++ Week 0/Week 0 ~ canI.js
@@ -1,5 +1,5 @@
 let differentTypes = [ 1, 'Hello', 2, 'World', true];
-if(differentTypes[4] = true) 
+if(differentTypes[4] == true) 
 {console.log( 'Yes, we can store different types in a single array' + differentTypes[1] + differentTypes[3])
 }
 
