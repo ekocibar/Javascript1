f0cc034ae2024e311af814e1d02da677a85b4a70
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-09 00:27:02 +0200

    selecting values from objects <for(let c in list)> //array way

--- Week1/10,11 selectValues.js
+++ Week1/10,11 selectValues.js
@@ -14,9 +14,13 @@ console.log("Amazing Joe's Garage, we service " + vehicles);
 
 
 
-               // array way
-
+               // array way  // with <and> word
+let vehicles2 =  "";
 let list2 = [ "motorbike", "caravan", "bike" ];
 for(let i = 0 ; i < list2.length ; i++){
-    console.log("Amazing Joe's Garage, we service " + list2[i]);
+    vehicles += ", " + list2[i];
+    if(i == list2.length-2){
+        vehicles += ", and" ;
+    }
 }
+console.log("Amazing Joe's Garage, we service " + vehicles2);
