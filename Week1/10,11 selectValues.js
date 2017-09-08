463ab014138c79591aadb684f7c1708e4cdc5187
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-09 00:14:27 +0200

    selecting values from objects <for(let c in list)> // array

--- Week1/10,11 selectValues.js
+++ Week1/10,11 selectValues.js
@@ -11,3 +11,12 @@ for(let c in list){
 }
 
 console.log("Amazing Joe's Garage, we service " + vehicles);
+
+
+
+               // array way
+
+let list2 = [ "motorbike", "caravan", "bike" ];
+for(let i = 0 ; i < list2.length ; i++){
+    console.log("Amazing Joe's Garage, we service " + list2[i]);
+}
