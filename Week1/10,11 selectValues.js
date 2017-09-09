aaad5f0a27350096b6de9f9f853207072c529332
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-09 08:06:06 +0200

    arrays // for loop

--- Week1/10,11 selectValues.js
+++ Week1/10,11 selectValues.js
@@ -1,26 +1,10 @@
-let list = {
-    l1 : "motorbike", 
-    l2 : "caravan", 
-    l3 : "bike" 
-}
-
-let vehicles =  "";
-
-for(let c in list){
-    vehicles += ", " + list[c];
-}
-
-console.log("Amazing Joe's Garage, we service " + vehicles);
-
-
-
-               // array way  // with <and> word
 let vehicles2 =  "";
 let list2 = [ "motorbike", "caravan", "bike" ];
+
 for(let i = 0 ; i < list2.length ; i++){
-    vehicles += ", " + list2[i];
+    vehicles2 += ", " + list2[i];
     if(i == list2.length-2){
-        vehicles += ", and" ;
+        vehicles2 += ", and" ;
     }
 }
 console.log("Amazing Joe's Garage, we service " + vehicles2);
