4c389537b0ff8d5d6e4f695bdd960c595e92bf46
Enes <30447759+pesh12@users.noreply.github.com>
2017-09-11 11:28:47 +0200

    array of strings

---
+++ Week3/bookNames.js
@@ -0,0 +1,17 @@
+        //####  1  ####\\
+console.log("\t#### 1 ####\n\n");
+/*Open a new js file and start by declaring in array with in there 10 strings. 
+These strings should be of book title's you have read (or made up) and be lowercase 
+without spaces or special characters so that you can use these later as Id's. 
+(Example: Harry Potter's - The Chamber of Secrets -> harry_potter_chamber_secrets).*/
+
+let bookNames = ["a_game_of_thrones", 
+                 "a_clash_of_kings", 
+                 "a_storm_of_swords", 
+                 "a_feast_for_crows", 
+                 "a_dance_with_dragons", 
+                 "the_winds_of_winter", 
+                 "a_dream_of_spring", 
+                 "the_fellowship_of_the_ring", 
+                 "the_two_towers", 
+                 "the_return_of_the_king"];
